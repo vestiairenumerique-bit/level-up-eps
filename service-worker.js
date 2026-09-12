83a8deebbe78401735223c1e456a31318cbf63c4
vestiairenumerique-bit <vestiairenumerique@gmail.com>
2026-09-12 14:51:06 +0200

    Version finale

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,7 @@
 const CACHE_NAME = "level-up-eps-v12";
-const SPORTS = ["athletics", "dance", "swimming", "team", "combat", "neutral"];
 const NEUTRAL_STATES = ["idle", "plus-one", "encourage-2", "streak-3", "almost-4", "perfect-5", "level-up", "return", "jumping", "knees", "hops", "squat", "wall", "plank", "lunges", "burpee", "balance", "stairs", "walk", "carry"];
 const APP_FILES = [
   "./", "./index.html", "./manifest.webmanifest", "./logo-app.png", "./logo-wordmark.png", "./icon-192.png", "./icon-512.png",
-  ...SPORTS.map(sport => `./assets/sports/${sport}.png`),
   ...NEUTRAL_STATES.map(state => `./assets/neutral-states/${state}.png`)
 ];
 
