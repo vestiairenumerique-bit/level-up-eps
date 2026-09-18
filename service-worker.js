741cd90aedac91f6cf944c4669c0ba58e6c35677
vestiairenumerique-bit <vestiairenumerique@gmail.com>
2026-09-18 17:31:12 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,10 @@
-const CACHE_NAME = "level-up-eps-v19";
+const CACHE_NAME = "level-up-eps-v21";
 const DEMONSTRATIONS = ["jumping", "knees", "hops", "squat", "wall", "plank", "lunges", "burpee", "balance", "stairs", "walk", "carry"];
+const BADGES = ["01-rookie","02-espoir","03-challenger","04-titulaire","05-pro","06-expert","07-leader","08-capitaine","09-champion","10-elite","11-icone","12-legende"].map(name => `./assets/badges/${name}.webp`);
+const REWARDS = ["bag-closed","bag-open","bottle"].map(name => `./assets/rewards/${name}.webp`);
 const APP_FILES = [
   "./", "./index.html", "./manifest.webmanifest", "./logo-app.png", "./logo-wordmark.png", "./icon-192.png", "./icon-512.png",
+  ...BADGES, ...REWARDS,
   ...DEMONSTRATIONS.map(state => `./assets/demonstrator/${state}.webp`),
   "./assets/demonstrator/burpee-v2.png",
   "./assets/demonstrator/pushup.png",
